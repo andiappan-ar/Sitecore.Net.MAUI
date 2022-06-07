8dc0e693acff92a0bfbc7f0b1a83edc2dbaa40b6
Andiappan <ARandi@outlook.com>
2022-06-07 15:38:27 +0400

    All basic component creation

---
+++ Src/DemoBank/Sitecore.MAUI.Blazor.DemoBank/Models/ViewModel/CardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.MAUI.RenderingEngine.Model.SCFields;
+
+namespace Sitecore.MAUI.Blazor.DemoBank.Models.ViewModel
+{
+    public class CardViewModel
+    {
+        public SCSingleLineText CardTitle { get; set; }
+        public SCRichText CardDescription { get; set; }
+        public SCSingleLineText CardParagraph { get; set; }
+        public SCImage CardImage { get; set; }
+        public SCLink CardLink { get; set; }
+        public List<SCMultiList<CardViewModel>> Elements { get; set; }
+    }
+
+}
--- Src/RazorClassClient/Sitecore.MAUI.RazorClassClient/Shared/Components/Fields/SCImage.razor.cs
+++ Src/RazorClassClient/Sitecore.MAUI.RazorClassClient/Shared/Components/Fields/SCImage.razor.cs
@@ -5,7 +5,13 @@ namespace Sitecore.MAUI.RazorClassClient.Shared.Components.Fields
     public partial class SCImage
     {
         [Parameter]
-        public Sitecore.MAUI.RenderingEngine.Model.SCFields.SCImage? scmodel { get; set; }     
+        public Sitecore.MAUI.RenderingEngine.Model.SCFields.SCImage? scmodel { get; set; }
+
+        [Parameter]
+        public string? customClass { get; set; }
+
+        [Parameter]
+        public string? customStyle { get; set; }
 
     }
 
